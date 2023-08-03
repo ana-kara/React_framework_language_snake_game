9e8cb1ad8a32b3226e7c3b758ce77b2ce74a7626
ana-kara <70655165+ana-kara@users.noreply.github.com>
2023-08-03 16:05:38 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,11 @@
+import React from 'react'
 
+function Footer(){
+    return (
+        <footer className="bg-gray-200 text-center text-xs p-3 fixed bottom-0 w-full border-t">
+            &copy; Copyright 2023
+        </footer>
+    )
+}
+
+export default Footer
