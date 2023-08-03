a5b402d4469146e52e48705a5299bf55f26bd265
ana-kara <70655165+ana-kara@users.noreply.github.com>
2023-08-03 15:58:35 +0300

    Update convert.py

--- products/convert.py
+++ products/convert.py
@@ -1,1 +1,11 @@
+from docx2json import convert
 
+def convert_doc_to_json(file_path):
+    json_data = convert(file_path)
+    return json_data
+
+# Usage example
+file_path = "C:\\xampp\\htdocs\\react-games\\public\\products\\Japanese verbs N5 level.docx"
+json_data = convert_doc_to_json(file_path)
+with open('Japanese verbs N5 level.json', 'w') as json_file:
+        json_file.write(json_data)
